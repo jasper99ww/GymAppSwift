0ae9f91e8e2f0637f01b089fb1e4ea9c7103e86b
Kacper P <kacperus4@onet.eu>
2021-07-07 01:04:23 +0200

    layout for calories calcuator done, all without upickerview done

--- GymLog/Profile/Body weight/Body Weight Calendar/BodyWeightCalories.swift
+++ GymLog/Profile/Body weight/Body Weight Calendar/BodyWeightCalories.swift
@@ -9,8 +9,6 @@ import UIKit
 
 class BodyWeightCalories: UIViewController {
 
-    
-    
     @IBOutlet weak var heightView: UIView!
     @IBOutlet weak var heightSlider: UISlider!
     @IBOutlet weak var heightValueLabel: UILabel!
@@ -41,22 +39,43 @@ class BodyWeightCalories: UIViewController {
     var gender = String()
     var height = Int()
     var weight = Int()
-    
+    var age = 25
+    var newAge = String()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
     makeTintImages()
     buttonsCorneRadius()
-        
+        genderViewsButton()
         heightSlider.setValue(170, animated: true)
         weightSlider.setValue(80, animated: true)
     }
     
+    func makeTintImages() {
+        guard let manGenderImage = UIImage(named: "manGender") else { return }
+        let tintManImage = manGenderImage.withRenderingMode(.alwaysTemplate)
+        manGender.image = tintManImage
+        manGender.tintColor = .green
+        manGenderView.layer.cornerRadius = 15
+        
+        guard let womanGenderImage = UIImage(named: "womanGender") else { return }
+        let tintWomanGender = womanGenderImage.withRenderingMode(.alwaysTemplate)
+        womanGender.image = tintWomanGender
+        womanGender.tintColor = .systemPink
+        womanGenderView.layer.cornerRadius = 15
+        
+        heightView.layer.cornerRadius = 15
+        weightView.layer.cornerRadius = 15
+        ageView.layer.cornerRadius = 15
+        activityView.layer.cornerRadius = 15
+        
+    }
     
     //MARK:- GENDER VIEWS
     
     func genderViewsButton() {
+        manGenderView.isUserInteractionEnabled = true
         let maleButton = UITapGestureRecognizer(target: self, action: #selector(maleButtonTapped))
         manGenderView.addGestureRecognizer(maleButton)
         
@@ -65,20 +84,37 @@ class BodyWeightCalories: UIViewController {
     }
     
    @objc func maleButtonTapped() {
+    manGender.alpha = 0.5
         gender = "male"
+    print("man tapped ")
     }
     
     @objc func femaleButtonTapped() {
         gender = "female"
+        womanGenderView.alpha = 0.5
+        print("women tapped")
     }
     
     
     //MARK:- SLIDERS
     
+    func cmToFootAndInches(_ cm: Double) -> String {
+        
+        let feet = cm * 0.0328084
+        let feetFloor = Int(floor(feet))
+        let feetRest: Double = ((feet * 100).truncatingRemainder(dividingBy: 100) / 100)
+        let inches = Int(floor(feetRest * 12))
+        
+        return "\(feetFloor)' \(inches)\""
+    }
+    
     @IBAction func heightSliderChanged(_ sender: UISlider) {
         height = Int(sender.value)
-        var heightInInch = String(format: "%.2f", (sender.value / 0.39370))
-        heightValueLabel.text = "\(String(height))cm (\(heightInInch)\"\")"
+        print("height is \(sender.value / 2.54)")
+        let heightInInch = cmToFootAndInches(Double(sender.value))
+        heightValueLabel.text = "\(String(height))cm (\(heightInInch))"
+        
+      
     }
     
     @IBAction func weightSliderChanged(_ sender: UISlider) {
@@ -86,7 +122,20 @@ class BodyWeightCalories: UIViewController {
         weightValueLabel.text = "\(String(weight))\(weightUnit)"
     }
     
-
+    @IBAction func minusButtonTapped(_ sender: UIButton) {
+        guard age >= 1 else { return print("za maly wiek")}
+        age -= 1
+        newAge = String(age)
+        ageTextField.text = newAge
+    }
+    
+    @IBAction func plusButtonTapped(_ sender: UIButton) {
+        guard age >= 1 else { return print("za maly wiek")}
+        age += 1
+        newAge = String(age)
+        ageTextField.text = newAge
+    }
+    
     //MARK:- UI SET UP
     func buttonsCorneRadius() {
         let greenColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1).cgColor
@@ -101,25 +150,7 @@ class BodyWeightCalories: UIViewController {
         agePlusButton.layer.borderWidth = 1
     }
     
-    func makeTintImages() {
-        guard let manGenderImage = UIImage(named: "manGender") else { return }
-        let tintManImage = manGenderImage.withRenderingMode(.alwaysTemplate)
-        manGender.image = tintManImage
-        manGender.tintColor = .darkGray
-        manGenderView.layer.cornerRadius = 15
-        
-        guard let womanGenderImage = UIImage(named: "womanGender") else { return }
-        let tintWomanGender = womanGenderImage.withRenderingMode(.alwaysTemplate)
-        womanGender.image = tintWomanGender
-        womanGender.tintColor = .darkGray
-        womanGenderView.layer.cornerRadius = 15
-        
-        heightView.layer.cornerRadius = 15
-        weightView.layer.cornerRadius = 15
-        ageView.layer.cornerRadius = 15
-        activityView.layer.cornerRadius = 15
-        
-    }
+   
 
 
 }
